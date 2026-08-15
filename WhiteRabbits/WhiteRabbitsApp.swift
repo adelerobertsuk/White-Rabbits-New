b5a3ba293a13f146b2e63ded3e5460726040c987
adelerobertsuk <adeleroberts.uk@gmail.com>
2026-08-15 08:31:07 +0100

    Initial Commit

---
+++ WhiteRabbits/WhiteRabbitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteRabbitsApp.swift
+//  WhiteRabbits
+//
+//  Created by Adele Roberts on 15/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct WhiteRabbitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
